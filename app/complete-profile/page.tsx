75b1a6102a3c0c356d11bc89144aee560cc3c901
Saeed Torkaman <imsaetor@gmail.com>
2026-04-20 15:55:08 +0330

    add complete profile page

---
+++ app/complete-profile/page.tsx
@@ -0,0 +1,16 @@
+import { redirect } from "next/navigation";
+import SetUsernameForm from "@/components/auth/set-username-form";
+import { getUser } from "@/lib/dal";
+
+const CompleteProfilePage = async () => {
+  const user = await getUser();
+  if (user.username) return redirect(`/${user.username}`);
+
+  return (
+    <div className="flex items-center justify-center flex-col h-svh p-4">
+      <SetUsernameForm />
+    </div>
+  );
+};
+
+export default CompleteProfilePage;
